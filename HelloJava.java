2af3c41886a3406af56e5af7f91cc8fb9f93a675
Moamen <moamen.gomaa.95@gmail.com>
2022-12-02 15:36:17 +0200

    added hellogit

--- HelloJava.java
+++ HelloJava.java
@@ -4,6 +4,10 @@ class HelloWorld{
 public void PrintHelloWorld(){
 System.out.println("Hello World");
 }
+class HelloGIT{
+public void PrintHelloWorld(){
+System.out.println("Hello World");
+}
 public static void main(String[] arg){
 
 PrintHelloWorld();
