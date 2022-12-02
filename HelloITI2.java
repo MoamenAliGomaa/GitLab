33dfa5a981025214fce84422a0a3c9b7fa610818
Moamen <moamen.gomaa.95@gmail.com>
2022-12-02 15:35:11 +0200

    added hello iti

--- HelloITI2.java
+++ HelloITI2.java
@@ -4,6 +4,9 @@ package GIT;
 public void PrintHelloGIT(){
 System.out.println("Hello GIT");
 }
+public void PrintHelloITI(){
+System.out.println("Hello GIT");
+}
 public static void main(String[] arg){
 
 PrintHelloITI();
