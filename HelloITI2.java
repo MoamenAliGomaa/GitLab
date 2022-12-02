b664b894acdce8480d161be0c91215048623a416
Moamen <moamen.gomaa.95@gmail.com>
2022-12-02 14:47:17 +0200

    Added PrintHelloGIT

--- HelloITI2.java
+++ HelloITI2.java
@@ -4,6 +4,9 @@ class HelloITI{
 public void PrintHelloITI(){
 System.out.println("Hello ITI");
 }
+public void PrintHelloGIT(){
+System.out.println("Hello GIT");
+}
 public static void main(String[] arg){
 
 PrintHelloITI();
